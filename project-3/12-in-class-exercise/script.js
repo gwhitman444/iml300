6813522ed96f95ad704bf438337070870f48233a
gwhitman444 <gwhitman@usc.edu>
2022-04-12 20:22:05 -0700

    demo

---
+++ project-3/12-in-class-exercise/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {   
+    $('#flower').click(function () {
+      $('p').toggle();
+    });
+    
+      $(function () {
+        $("#flower").draggable();
+    });   
+//     $('#flower').click(function () {
+////        $('.infopanel').toggle(2000);     
+//        $(this).toggleClass("bigger");
+//    });
+    
+});

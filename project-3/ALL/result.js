94c614b0edac504743c5b5cb00d83b32ccacd96c
gwhitman444 <gwhitman@usc.edu>
2022-04-25 22:29:14 -0700

    4/25

--- project-3/ALL/result.js
+++ project-3/ALL/result.js
@@ -11,3 +11,58 @@ $(document).ready(function() {
       }); 
    });
 });
+
+
+let particles = [];
+var canvas;
+
+function setup() {
+createCanvas(window.innerWidth, window.innerHeight)
+  for (let i = 0; i < 10; i++) {
+    particles[i] = new Particle();
+  }
+}
+
+function draw() {
+background(255, 255, 255);
+  
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].move();
+    particles[i].show();
+  }
+}
+
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.speed = createVector(random(-10, 10), random(-10, 10));
+    this.c = color(random(50), random(200), random(200));
+    this.r = random(10, 100);
+  }
+  
+  move() {
+    
+    this.pos.add(this.speed);
+    
+    if (this.pos.x < 0) {
+      this.speed.x = abs(this.speed.x);
+    } else if (this.pos.x > width) {
+      this.speed.x = -abs(this.speed.x);
+    }
+    
+    if (this.pos.y < 0) {
+      this.speed.y = abs(this.speed.y);
+    } else if (this.pos.y > height) {
+      this.speed.y = -abs(this.speed.y);
+    }
+    
+  }
+  
+  show() {
+    noStroke();
+    fill(this.c);
+    ellipse(this.pos.x, this.pos.y, this.r);
+  }
+
+
+}
---
+++ project-3/ALL/thanks.js
@@ -0,0 +1,19 @@
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav2").style.height = "100%";
+    document.getElementById("myNav3").style.height = "100%";
+    document.getElementById("myNav4").style.height = "100%";
+    document.getElementById("myNav5").style.height = "100%";
+    document.getElementById("myNav6").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+document.getElementById("myNav").style.height = "0%";
+document.getElementById("myNav2").style.height = "0%";
+document.getElementById("myNav3").style.height = "0%";
+document.getElementById("myNav4").style.height = "0%";
+document.getElementById("myNav5").style.height = "0%";
+document.getElementById("myNav6").style.height = "0%";
+}
